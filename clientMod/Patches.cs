c84307601a31fe27b494a9e15f574e3a714d3a72
MarsyApp <MarsyApp>
2024-05-12 17:13:06 +0700

    fix sell error

--- clientMod/Patches.cs
+++ clientMod/Patches.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using Aki.Reflection.Patching;
+using EFT;
 using EFT.InventoryLogic;
 using EFT.UI;
+using EFT.UI.DragAndDrop;
 using HarmonyLib;
 
 namespace EnableSaveCaseForScav
@@ -29,8 +32,12 @@ namespace EnableSaveCaseForScav
             this._patches = new List<ModulePatch>
             {
                 new ItemViewPatches.PostRaidHealthScreenClassPath(),
-                new ItemViewPatches.InteractionsHandlerClassPath(),
+                // new ItemViewPatches.InteractionsHandlerClassPath(),
                 new ItemViewPatches.HasItemsPath(),
+                // new ItemViewPatches.ScavengerInventoryScreen_method_3_Path(),
+                new ItemViewPatches.SlotView_CanDrag_Path(),
+                new ItemViewPatches.ItemUiContext_QuickFindAppropriatePlace_Path(),
+                // new ItemViewPatches.SlotViewHeader_Show_Path(),
             };
         }
 
@@ -56,12 +63,18 @@ namespace EnableSaveCaseForScav
     public static class ItemViewPatches
     {
 
-        private static string caseId = "5732ee6a24597719ae0c0281";
-        
+        private static bool isScav(IProfile profile)
+        {
+            return profile.Side == EPlayerSide.Savage;
+        }
         public class PostRaidHealthScreenClassPath : ModulePatch
         {
+            // показ окна передачи лута даже при смерти, если есть подсумок
+            
+            protected static FieldInfo profileFieldInfo;
             protected override MethodBase GetTargetMethod()
             {
+                profileFieldInfo = AccessTools.GetDeclaredFields(typeof(PostRaidHealthScreenClass)).Single(x => x.FieldType == typeof(Profile));
                 return AccessTools.PropertyGetter(typeof(PostRaidHealthScreenClass), nameof(PostRaidHealthScreenClass.Boolean_3));
             }
 
@@ -70,7 +83,14 @@ namespace EnableSaveCaseForScav
             {
                 try
                 {
-                    __result = true;
+                    Profile profile = (Profile)profileFieldInfo.GetValue(__instance);
+                    IEnumerable<Item> allRealPlayerItems = profile.Inventory.AllRealPlayerItems; 
+                    bool hasSecureContainerTemplateClass = allRealPlayerItems.Any(x => x.Template.Parent is SecureContainerTemplateClass); 
+
+                   if (isScav(profile) && hasSecureContainerTemplateClass)
+                   {
+                       __result = true;
+                   }
                 } catch (Exception e)
                 {
                     Logger.LogError(e);
@@ -78,33 +98,78 @@ namespace EnableSaveCaseForScav
             }
         }
         
-        public class InteractionsHandlerClassPath : ModulePatch
+        /*public class InteractionsHandlerClassPath : ModulePatch
         {
+            
+            // блокировка перемещения подсумка в инвентарь
             protected override MethodBase GetTargetMethod()
             {
-                return AccessTools.Method(typeof(InteractionsHandlerClass), nameof(InteractionsHandlerClass.Remove));
+                return AccessTools.Method(typeof(InteractionsHandlerClass), nameof(InteractionsHandlerClass.Move));
             }
 
-            [PatchPostfix]
-            private static void PatchPostfix(InteractionsHandlerClass __instance, Item item, ref GStruct414<GClass2785> __result)
+            [PatchPrefix]
+            private static bool PatchPrefix(InteractionsHandlerClass __instance, Item item, TraderControllerClass itemController, ref GStruct414<GClass2786> __result)
             {
                 try
                 {
-                    if (item.TemplateId.Equals(caseId))
+                    Debugger.Break();
+                    bool isScav = (itemController as GClass2759).Side == EPlayerSide.Savage || true;
+                    bool isSecureContainer = item.Template.Parent is SecureContainerTemplateClass;
+                    if (isScav && isSecureContainer)
                     {
                         UnlootableComponent itemComponent = item.GetItemComponent<UnlootableComponent>();
-                        __result = new InteractionsHandlerClass.GClass3334(itemComponent);
+                        // __result = new InteractionsHandlerClass.GClass3334(itemComponent);
+                        __result = new InteractionsHandlerClass.GClass3334(null);
+                        return false;
                     }
 
                 } catch (Exception e)
                 {
                     Logger.LogError(e);
                 }
+
+                return true;
             }
-        }
+        }*/
+        
+        /*public class InteractionsHandlerClassPath : ModulePatch
+        {
+            
+            // блокировка перемещения подсумка в инвентарь
+            protected override MethodBase GetTargetMethod()
+            {
+                return AccessTools.Method(typeof(InteractionsHandlerClass), nameof(InteractionsHandlerClass.Remove));
+            }
+
+            [PatchPrefix]
+            private static bool PatchPrefix(InteractionsHandlerClass __instance, Item item, TraderControllerClass itemController, bool simulate, bool ignoreRestrictions, ref GStruct414<GClass2785> __result)
+            {
+                try
+                {
+                    Logger.LogInfo($"InteractionsHandlerClass.Remove simulate: ${simulate}");
+                    if (simulate) return true;
+                    bool isScav = (itemController as GClass2759).Side == EPlayerSide.Savage || true;
+                    bool isSecureContainer = item.Template.Parent is SecureContainerTemplateClass;
+                    if (isScav && isSecureContainer)
+                    {
+                        UnlootableComponent itemComponent = item.GetItemComponent<UnlootableComponent>();
+                        // __result = new InteractionsHandlerClass.GClass3334(itemComponent);
+                        __result = (GStruct414<GClass2785>) (Error) new InteractionsHandlerClass.GClass3334(itemComponent);
+                        return false;
+                    }
+
+                } catch (Exception e)
+                {
+                    Logger.LogError(e);
+                }
+
+                return true;
+            }
+        }*/
         
         public class HasItemsPath : ModulePatch
         {
+            // не показывать предупреждение о продаже если остался только подсумок
 
             protected static Type nestedType;
             protected override MethodBase GetTargetMethod()
@@ -123,7 +188,7 @@ namespace EnableSaveCaseForScav
                     GClass2764 scavController = (GClass2764)scavControllerFiledInfo.GetValue(__instance);
                     IEnumerable<Item> items = scavController.Inventory.AllRealPlayerItems;
                     
-                    IEnumerable<Item> filteredItems = items.Where(x => !x.TemplateId.Equals(caseId));
+                    IEnumerable<Item> filteredItems = items.Where(x => !(x.Template.Parent is SecureContainerTemplateClass));
                     int filteredCount = filteredItems.Count();
                     if (filteredCount == 0)
                     {
@@ -136,5 +201,143 @@ namespace EnableSaveCaseForScav
                 }
             }
         }
+        
+        /*public class ScavengerInventoryScreen_method_3_Path : ModulePatch
+        {
+            
+            // ? блокировка кнопки продать все, если нет ничего кроме подсумка
+            protected static Type nestedType;
+            protected override MethodBase GetTargetMethod()
+            {
+                nestedType = AccessTools.Inner(typeof(ScavengerInventoryScreen), nameof(ScavengerInventoryScreen.GClass3131));
+                return AccessTools.GetDeclaredMethods(typeof(ScavengerInventoryScreen)).Single(x => x.ReturnType == typeof(bool) && x.GetParameters().Length == 1 && x.GetParameters()[0].ParameterType == typeof(IEnumerable<Item>).MakeByRefType());
+            }
+
+            [PatchPostfix]
+            private static void PatchPostfix(ScavengerInventoryScreen __instance, ref bool __result)
+            {
+
+                try
+                {
+                    
+                    __result = false;
+                    FieldInfo scavControllerFiledInfo = AccessTools.Field(nestedType, "ScavController");
+                    GClass2764 scavController = (GClass2764)scavControllerFiledInfo.GetValue(__instance);
+                    IEnumerable<Item> items = scavController.Inventory.AllRealPlayerItems;
+                    
+                    IEnumerable<Item> filteredItems = items.Where(x => !(x.Template.Parent is SecureContainerTemplateClass));
+                    int filteredCount = filteredItems.Count();
+                    if (filteredCount == 0)
+                    {
+                        __result = false;
+                    }
+                } catch (Exception e)
+                {
+                    Logger.LogError(e);
+                }
+            }
+        }*/
+        
+        public class SlotView_CanDrag_Path : ModulePatch
+        {
+            // запрет на перетаскивание подсумка в склад
+            protected static MethodInfo slotFiledInfo;
+            protected override MethodBase GetTargetMethod()
+            {
+                slotFiledInfo = AccessTools.PropertyGetter(typeof(SlotView), nameof(SlotView.Slot));
+                return AccessTools.Method(typeof(SlotView), nameof(SlotView.CanDrag));
+            }
+
+            [PatchPrefix]
+            private static bool PatchPrefix(SlotView __instance, ItemContextAbstractClass itemContext, ref bool __result)
+            {
+                try
+                {
+                    Slot slot = (Slot)slotFiledInfo.Invoke(__instance, null);
+                    var ParentItem = slot.ParentItem;
+                    InventoryControllerClass Owner = ParentItem.Owner as InventoryControllerClass;
+                    IProfile Profile = Owner.Profile;
+                    bool isSecureContainer = itemContext.Item.Template.Parent is SecureContainerTemplateClass;
+                    if (isScav(Profile) && isSecureContainer)
+                    {
+                        __result = false;
+                        return false;
+                    }
+
+                } catch (Exception e)
+                {
+                    Logger.LogError(e);
+                }
+
+                return true;
+            }
+        }
+        
+        public class ItemUiContext_QuickFindAppropriatePlace_Path : ModulePatch
+        {
+            // запрет на перенос посука через ctrl+click
+            protected static FieldInfo profileFieldInfo;
+            protected override MethodBase GetTargetMethod()
+            {
+                profileFieldInfo = AccessTools.GetDeclaredFields(typeof(ItemUiContext)).Single(x => x.FieldType == typeof(Profile));
+                return AccessTools.Method(typeof(ItemUiContext), nameof(ItemUiContext.QuickFindAppropriatePlace));
+            }
+
+            [PatchPrefix]
+            private static bool PatchPrefix(ItemUiContext __instance, ItemContextAbstractClass itemContext, ref GStruct413 __result)
+            {
+                try
+                {
+                    Debugger.Break();
+                    InventoryControllerClass Owner = itemContext.Item.Owner as InventoryControllerClass;
+                    IProfile profile = Owner.Profile;
+                    bool isSecureContainer = itemContext.Item.Template.Parent is SecureContainerTemplateClass;
+                    Debugger.Break();
+                    if (isScav(profile) && isSecureContainer)
+                    {
+                        Debugger.Break();
+                        __result = new GClass3346("Can't find appropriate container");
+                        return false;
+                    }
+                    Debugger.Break();
+
+                } catch (Exception e)
+                {
+                    Logger.LogError(e);
+                }
+                
+                return true;
+            }
+        }
+        
+        /*public class SlotViewHeader_Show_Path : ModulePatch
+        {
+            // выключение кнопки смены предмета в слоте
+            // похоже не нужен
+            protected override MethodBase GetTargetMethod()
+            {
+                return AccessTools.Method(typeof(SlotViewHeader), nameof(SlotViewHeader.Show));
+            }
+
+            [PatchPrefix]
+            private static bool PatchPrefix(InventoryControllerClass inventoryController, Slot slot, ref bool canBeSelected)
+            {
+                try
+                {
+                    // Debugger.Break();
+                    bool isSecureContainer = slot.ID.Equals("SecuredContainer");
+                    if (isScav(inventoryController.Profile) && isSecureContainer)
+                    {
+                        canBeSelected = false;
+                    }
+
+                } catch (Exception e)
+                {
+                    Logger.LogError(e);
+                }
+                
+                return true;
+            }
+        }*/
     }
 }
